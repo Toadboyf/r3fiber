de35e234d1f30e3f47e7d7e0ef0e946d28e2f539
ZMartensFMK <zach.martens@fmk.agency>
2023-12-17 00:04:57 -0600

    updating camera zoom on mobile breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import './App.scss'
 import { CameraControls, Cloud, Clouds, Environment, Scroll, ScrollControls, Stars, useScroll } from '@react-three/drei'
 import { Ollie } from './components/Ollie'
 import { Dungeon } from './components/Dungeon'
-import { Vector3 } from 'three'
+import { Vector2, Vector3 } from 'three'
+import { lerp } from 'three/src/math/MathUtils'
 
 function App() { 
 
   return (
     <div id='canvas-container'>
       <Canvas camera={{position: [0,0,0]}}>
+        <MobileUpdate />
         {/* <ambientLight intensity={0.1} /> */}
         {/* <directionalLight /> */}
         {/* <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} /> */}
@@ -33,6 +35,7 @@ function App() {
 
         <Dungeon position={[0,-1,0]} />
         <Rig />
+        
       </Canvas>
     </div>
   )
@@ -45,6 +48,18 @@ function Rig() {
     camera.position.lerp(vec.set(state.pointer.x, state.pointer.y/2, camera.position.z), 0.02);
     camera.lookAt(0,0,-6);
   })
+}
+
+function MobileUpdate() {
+  const { camera, size } = useThree();
+  useFrame(() => {
+    if(size.width <= 470) {//mobile breakpoint
+      camera.zoom = 0.5;
+    } else {
+      camera.zoom = 1;
+    }
+  })
+  
 }
 
 export default App
